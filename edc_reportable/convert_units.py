946b433a6f1c53191ce6192c598a249e5ec850bd
erikvw <ew2789@gmail.com>
2019-11-07 23:32:37 -0600

    check units have value

--- edc_reportable/convert_units.py
+++ edc_reportable/convert_units.py
@@ -8,7 +8,7 @@ class ConversionNotHandled(Exception):
 def convert_units(value, units_from=None, units_to=None, places=None):
     places = places or 4
     converted_value = value
-    if value and units_from != units_to:
+    if value and units_from and units_to and units_from != units_to:
         if (units_from, units_to) == (MILLIGRAMS_PER_DECILITER, MILLIMOLES_PER_LITER):
             converted_value = float(value) / 18.018018
         elif (units_from, units_to) == (MILLIMOLES_PER_LITER, MILLIGRAMS_PER_DECILITER):
