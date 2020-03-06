9447bbd10e32005604a622318d860d9859aaacc9
erikvw <ew2789@gmail.com>
2020-03-06 09:49:02 -0600

    test setup

--- setup.py
+++ setup.py
@@ -4,39 +4,39 @@ from setuptools import setup
 from setuptools import find_packages
 from os.path import join, abspath, normpath, dirname
 
-with open(join(dirname(__file__), 'README.rst')) as readme:
+with open(join(dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
-with open(join(dirname(__file__), 'VERSION')) as f:
+with open(join(dirname(__file__), "VERSION")) as f:
     VERSION = f.read()
 
 # allow setup.py to be run from any path
 os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
 setup(
-    name='edc-reportable',
+    name="edc-reportable",
     version=VERSION,
-    author=u'Erik van Widenfelt',
-    author_email='ew2789@gmail.com',
+    author=u"Erik van Widenfelt",
+    author_email="ew2789@gmail.com",
     packages=find_packages(),
-    url='http://github.com/clinicedc/edc-reportable',
-    license='GPL license, see LICENSE',
-    description='Reportable clinic events, reference ranges, grading',
+    url="http://github.com/clinicedc/edc-reportable",
+    license="GPL license, see LICENSE",
+    description="Reportable clinic events, reference ranges, grading",
     long_description=README,
     include_package_data=True,
     zip_safe=False,
-    keywords='django Edc normal clinical reference ranges grading',
-    install_requires=['edc-utils', 'edc-constants'],
+    keywords="django Edc normal clinical reference ranges grading",
+    install_requires=[],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     python_requires=">=3.7",
 )
